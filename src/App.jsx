a6a0803cf328ed2a2a48877fa03fa12289fa34f6
AntonioEvandro <evandroalves123321@gmail.com>
2022-08-26 17:16:14 -0300

    4 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
-import 
+import CardPokemon from './components/CardPokemon';
+import api from './services/api';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <h1>Pokemons 1ª Geração</h1>
--- src/components/CardPokemon.jsx
+++ src/components/CardPokemon.jsx
@@ -2,7 +2,15 @@ const CardPokemon = function (props) {
   return (
     <div className="card ${tipo_base}">
       <span className="id ${tipo_base}">#${pokemon?.id}</span>
+      <img
+        src="${pokemon?.sprites.other.dream_world.front_default}"
+        alt=""
+        className="photo"
+      />
+      <span className="name ${tipo_base}">${pokemon?.name}</span>
+      <ul className="list_types">${tipos}</ul>
     </div>
   );
 };
+
 export default CardPokemon;
